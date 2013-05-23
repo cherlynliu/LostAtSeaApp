3697d33e9b8e243301efd42710f63d325263f4b4
cherlyn liu <cl_liu@hotmail.com>
2013-05-23 17:17:32 +0800

    修改wasd移動(可左右轉.後退慢)，加了小型地圖，camera綁在player裡面

--- LostAtSea/Assets/script/player_controller.cs
+++ LostAtSea/Assets/script/player_controller.cs
@@ -11,9 +11,10 @@ public class player_controller : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyUp(KeyCode.S)) BoatAndPlayer.transform.Translate(0, 0, -15 * Time.deltaTime);
-        if (Input.GetKeyUp(KeyCode.A)) BoatAndPlayer.transform.Translate(-15 * Time.deltaTime, 0, 0);
-        if (Input.GetKeyUp(KeyCode.D)) BoatAndPlayer.transform.Translate(15 * Time.deltaTime, 0, 0);
-        if (Input.GetKeyUp(KeyCode.W)) BoatAndPlayer.transform.Translate(0, 0, 15 * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S)) BoatAndPlayer.transform.Translate(0, 0, -5 * Time.deltaTime);
+        if (Input.GetKey(KeyCode.W)) BoatAndPlayer.transform.Translate(0, 0, 7.5f * Time.deltaTime);
+        //if (Input.GetKeyDown(KeyCode.A)) BoatAndPlayer.transform.Translate(-15 * Time.deltaTime, 0, 0);
+        if (Input.GetKey(KeyCode.A)) BoatAndPlayer.transform.Rotate(0, -30 * Time.deltaTime, 0);
+        if (Input.GetKey(KeyCode.D)) BoatAndPlayer.transform.Rotate(0, 30 * Time.deltaTime, 0);
 	}
 }
